53bfba10d5f8ae9b0c7b25b1a268d20560eb2d0d
danielle lane <daniellelane1821@gmail.com>
2019-09-17 15:33:36 -0500

    finished DO_WHILE_LOOPS part of exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,12 +1,30 @@
 public class ControlFlowExercises {
 
    public static void main (String[] args) {
+       //DO WHILE LOOPS
+//       int i = 100;
+//
+//       do {
+//           System.out.println(i);
+//             i  -=5;
+//       }while(i >= -10);
 
-       //LOOP BASICS
-       int i = 5;
-       while (i <= 15) {
+//       long i = 2;
+//
+//       do{
+//           System.out.println(i);
+//           i *= i;
+//       }while(i < 1000000);
+//       }
+
+       //refactor with 4 loop
+
+//       for (int i = 100; i >= -10; i -= 5) {
+//           System.out.println(i);
+//       }
+
+       for(long i = 2; i < 1000000; i*=i){
            System.out.println(i);
-           i++;
        }
    }
 }
