e46a4979a25161ee0698267f7f004811e546e69a
danielle lane <daniellelane1821@gmail.com>
2019-09-17 15:45:17 -0500

    finished the FIZZBUZZ exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,30 +1,24 @@
 public class ControlFlowExercises {
 
    public static void main (String[] args) {
-       //DO WHILE LOOPS
-//       int i = 100;
-//
-//       do {
-//           System.out.println(i);
-//             i  -=5;
-//       }while(i >= -10);
+    //FIZZBUZZ
 
-//       long i = 2;
-//
-//       do{
-//           System.out.println(i);
-//           i *= i;
-//       }while(i < 1000000);
-//       }
-
-       //refactor with 4 loop
-
-//       for (int i = 100; i >= -10; i -= 5) {
+//       for(int i = 1; i <= 100; i++){
 //           System.out.println(i);
 //       }
 
-       for(long i = 2; i < 1000000; i*=i){
-           System.out.println(i);
+       for(int i = 1; i <= 100; i++){
+            if(i == 15){
+                System.out.println("FIZZBUZZ");
+            }else if(i % 3 == 0){
+                System.out.println("FIZZ");
+            }else if(i % 5 == 0){
+                System.out.println("BUZZ");
+            }else{
+                System.out.println(i);
+            }
        }
+
+
    }
 }
