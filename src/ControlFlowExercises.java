01a3d9fb2ff4d1e09ed5d9e021712b9e413df376
danielle lane <daniellelane1821@gmail.com>
2019-09-18 10:57:17 -0500

    finished walk through

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -3,24 +3,31 @@ import java.util.Scanner;
 public class ControlFlowExercises {
 
    public static void main (String[] args) {
-    //GRADES
-      Scanner grade = new Scanner(System.in);
-       System.out.println("Enter grade");
-       int grades = grade.nextInt();
+       //GRADES
+       Scanner grade = new Scanner(System.in);
 
-       if (grades >= 88) {
-           System.out.println("You received an A");
-       } else if (grades >= 87) {
-           System.out.println("You received a B");
-       } else if (grades >= 79) {
-           System.out.println("You received a C");
-       } else if (grades >= 66) {
-           System.out.println("You received a D");
-       } else {
-           System.out.println("You received an F");
-       }
+
+       do {
+           System.out.println("Enter grade");
+           int grades = Integer.parseInt(grade.nextLine());
+
+           if (grades <= 100 && grades >= 88) {
+               System.out.println("You received an A");
+           } else if (grades <= 87 && grades >= 80) {
+               System.out.println("You received a B");
+           } else if (grades <= 79 && grades >= 67) {
+               System.out.println("You received a C");
+           } else if (grades <= 66 && grades >= 60) {
+               System.out.println("You received a D");
+           } else if (grades <= 59 && grades >= 0)
+               System.out.println("You received an F");
+
+
+       System.out.println("Enter another grade? Y/N");
+        } while (grade.nextLine().equalsIgnoreCase("y"));
+
+
 
    }
-
 
 }
