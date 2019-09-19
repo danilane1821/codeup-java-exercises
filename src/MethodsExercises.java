223782b4f8818b7d34d3c788f10578f602fc3add
danielle lane <daniellelane1821@gmail.com>
2019-09-19 11:57:48 -0500

    in the middle of solving problem

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class MethodsExercises {
 
     public static void main(String[] args) {
@@ -6,6 +8,11 @@ public class MethodsExercises {
         System.out.println(subtraction(15,3));
         System.out.println(division(20,5));
         System.out.println(multiplication(10,7));
+        System.out.println(modulus(2,4));
+
+        System.out.print("Enter a number between 1 and 10: ");
+        int userInput = getInteger(1, 10);
+
 
     }
 
@@ -24,6 +31,25 @@ public class MethodsExercises {
 
     public static double multiplication (double num1, double num2){
         return num1 * num2;
+    }
+
+    public static double modulus (double num1, double num2){
+        return num1 % num2;
+        //if we divide by 0 we get NaN...we should get...
+    }
+
+    // problem 2
+
+    public static int getInteger(int min, int max) {
+        Scanner input = new Scanner(System.in);
+
+        if(input.hasNextInt() && !input.hasNextDouble() && !input.hasNextFloat()) {
+            System.out.println("Lets check to see if its between 1 and 10");
+        }else{
+            System.out.println("That is not a valid number");
+
+        }
+        return min + max;
     }
 
 
