1f0e0dc572758e65194015f68e36eee086d8d8c0
danielle lane <daniellelane1821@gmail.com>
2019-09-19 10:49:17 -0500

    finished first part of first exercise

---
+++ src/MethodsExercises.java
@@ -0,0 +1,30 @@
+public class MethodsExercises {
+
+    public static void main(String[] args) {
+
+        System.out.println(addition(5,5));
+        System.out.println(subtraction(15,3));
+        System.out.println(division(20,5));
+        System.out.println(multiplication(10,7));
+
+    }
+
+
+    public static double addition (double num1, double num2){
+        return num1 + num2;
+    }
+
+    public static double subtraction (double num1, double num2){
+        return num1 - num2;
+    }
+
+    public static double division (double num1, double num2){
+        return num1 / num2;
+    }
+
+    public static double multiplication (double num1, double num2){
+        return num1 * num2;
+    }
+
+
+}
