3aa2a486ea692243e0cf7483cdaf55dffc0eb794
danielle lane <daniellelane1821@gmail.com>
2019-09-25 11:52:47 -0500

    fixed an error

--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -10,7 +10,7 @@ public class InputTest {
             System.out.println(test.yesNo());
             System.out.println(test.getInt(1,10));
             System.out.println(test.getDouble(1,10));
-            System.out.println(test.getInt());
+            System.out.println(test.getInt(""));
             System.out.println(test.getDouble(""));
 
         }
